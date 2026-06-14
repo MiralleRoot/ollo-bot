da9a089c1d54f2fc92b3b8d74298931a887bf652
Miralle <miralleroot@gmail.com>
2026-06-14 18:30:02 +0300

    init: ollo-bot skeleton

---
+++ main.py
@@ -0,0 +1,16 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+bot = Bot(token=os.getenv("BOT_TOKEN"))
+dp = Dispatcher()
+
+async def main():
+    print("oLLo запускается...")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
